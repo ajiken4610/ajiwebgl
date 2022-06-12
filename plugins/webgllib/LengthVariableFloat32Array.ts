5da727242695893cb534757d0e4e259823a93789
ajiken4610 <86188770+ajiken4610@users.noreply.github.com>
2022-06-12 18:25:28 +0900

    LengthVariableFloat32Arrayの実装を追加(未テスト)

--- plugins/webgllib/LengthVariableFloat32Array.ts
+++ plugins/webgllib/LengthVariableFloat32Array.ts
@@ -5,10 +5,17 @@ export class LengthVariableFloat32Array {
   private array: Float32Array;
   private _length: number;
   public get length(): number {
-    return 0;
+    return this._length;
   }
-  public push(arg: number): void {
-    return;
+  public push(value: number): void {
+    if (this.array.length <= this.length) {
+      let oldArray = this.array;
+      let newArray = new Float32Array(oldArray.length + 16);
+      newArray.set(oldArray);
+      this.array = newArray;
+    }
+    this.array[length] = value;
+    this._length++;
   }
   public toArray(): Float32Array {
     return this.array.slice(0, this.length);
